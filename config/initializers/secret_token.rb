478239a4ee6c8180579fd3a6aceec1bc93661825
Tong Ha <tong.d.ha@gmail.com>
2013-02-24 07:02:26 -0800

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afg::Application.config.secret_token = '7c0bd5ddd18c503f60969d0d0316b0d6de24f9adb1dfc5055bac9cbe48102e9ace6565a5358632723a9c87c366b9e53b93504531c246dd267f12eb2693e8734c'
