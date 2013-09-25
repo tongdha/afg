4e42c6f61eaaa2cc0d6f49461ef0477e0602b4a9
Tong Ha <tong.d.ha@gmail.com>
2013-09-24 17:49:32 -0700

    changed redirect_controller, changed text on home page

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -3,7 +3,7 @@ class RedirectController < ApplicationController
   def redirect
  #    request.headers['HTTP_REFERER'] = 'http://localhost2'
   #    redirect_to "#{params[:url]}"
-  @redirectUrl = "http://www.amazon.com/gp/product/" + params[:asin] + "?tag=pontpics-21%26linkCode=xm2"
+  @redirectUrl = "http://www.amazon.com/gp/product/" + params[:asin] + "?tag=araise-20%26linkCode=xm2%26camp=2025%26creative=165953%26creativeASIN=" + params[:asin]
   render :layout => false
   end
 end
